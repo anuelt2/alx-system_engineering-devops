172964ab6506bd81e434742d11585e58f2f1a784
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-10 15:29:47 +0000

    Add 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,16 @@
+# Create a file in /tmp
+file { '/tmp/school':
+  ensure  => 'present',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
+
+user { 'www-data':
+  ensure => 'present',
+}
+
+group { 'www-data':
+  ensure => 'present',
+}
