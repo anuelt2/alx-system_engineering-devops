7535804b911f7b5c5efed7a703eb3bcb57851904
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-14 22:25:57 +0000

    Modify 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -12,39 +12,15 @@ package { 'Nginx':
   require => Exec['apt_update'],
 }
 
-exec { 'ufw_allow_HTTP':
-  command => "sudo ufw allow 'Nginx HTTP'",
-  path    => '/usr/sbin/:/usr/bin/',
-  require => Package['Nginx'],
-}
-
-exec { 'ufw_allow_OpenSSH':
-  command => 'sudo ufw allow OpenSSH',
-  path    => '/usr/sbin/:/usr/bin/',
-  require => Exec['ufw_allow_HTTP'],
-}
-
-exec { 'ufw_enable':
-  command => 'sudo ufw enable',
-  path    => '/usr/sbin/:/usr/bin/',
-  unless  => "sudo ufw status | grep -q 'Status: active'",
-  require => Exec['ufw_allow_OpenSSH'],
-}
-
 file { '/var/www/html/index.html':
   ensure  => 'present',
   content => 'Hello World!',
-  require => Exec['ufw_enable'],
 }
 
 exec { 'add_redirect_directive':
-  command => @(CMD), REDIRECT_URL='https://www.youtube.com/watch?v=9t9Mp0BGnyI';
-  REDIRECT_BLOCK=$(printf '\\tlocation /redirect_me {\\\\n\\t\\treturn 301 %s;\\\\n\\t}' "$REDIRECT_URL");
-  sudo sed -i '/server_name _;/a\\\\'$REDIRECT_BLOCK'' /etc/nginx/sites-available/default
-  | CMD,
-  path    => '/bin/:/usr/bin/',
-  unless  => "grep -q 'location /redirect_me' /etc/nginx/sites-available/default",
-  require => Exec['ufw_enable'],
+  command  => "sudo sed -i '/server_name _;/a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=9t9Mp0BGnyI permanent;' /etc/nginx/sites-available/default",
+  path     => '/bin/:/usr/bin/',
+  provider => 'shell',
 }
 
 service { 'nginx':
