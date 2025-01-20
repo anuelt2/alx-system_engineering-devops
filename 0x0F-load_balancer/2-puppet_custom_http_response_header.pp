b5695c3311871c3ff9e1fc049d17aa8e0ea3ef72
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-01-20 22:55:50 +0000

    Modify 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -2,16 +2,15 @@
 # Create a custom HTTP header response
 
 exec { 'apt-update':
-  command  => '/usr/bin/apt-get update -y',
-  path     => ['/usr/sbin', '/usr/bin'],
+  command => '/usr/bin/apt-get -y update',
+  path    => ['/usr/sbin', '/usr/bin'],
 }
 
 package { 'nginx':
   ensure  => 'installed',
-  require => Exec['apt-update'],
 }
 
-file_line { 'custom_http_header':
+file_line { 'custom_http_response_header':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   line   => "\tadd_header X-Served-By \"${hostname}\";",
@@ -22,4 +21,10 @@ service { 'nginx':
   ensure  => 'running',
   enable  => true,
   require => Package['nginx'],
+}
+
+exec { 'restart_nginx':
+  command  => 'sudo service nginx restart',
+  provider => 'shell',
+  require  => File_line['custom_http_response_header']
 }
