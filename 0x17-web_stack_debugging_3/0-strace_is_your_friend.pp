6a8c1913de8fcbda4d0bb85a8b5d84e763b6e48a
Emmanuel Kwadwo Tettey <anuelt@gmail.com>
2025-02-26 13:21:17 +0000

    Modify 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,14 +4,16 @@ file { '/var/www/html/wp-settings.php':
   ensure => 'present',
 }
 
-exec { 'sed':
-  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+exec { 'fix_typo_wp_settings':
+  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
   path    => '/bin/:/usr/bin/:/usr/sbin/',
   require => File['/var/www/html/wp-settings.php'],
+  notify  => Service['apache2'],
 }
 
 service {'apache2':
   ensure     => 'running',
-  enable     => 'true',
-  hasrestart => 'true',
+  enable     => true,
+  hasrestart => true,
+  require    => Exec['fix_typo_wp_settings']
 }
